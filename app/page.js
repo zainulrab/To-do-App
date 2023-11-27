23b70808212a04373e0ba05c5a09c9d9ca2866c4
zain ul rab <zayn.rab604@gmail.com>
2023-11-28 00:24:13 +0500

    Complete and Responsive

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 "use client";
 import React, { useState } from 'react';
+//first breakpoint 712px
+//first breakpoint 712px
 
 
 const page = () =>
@@ -27,12 +29,12 @@ const page = () =>
         renderTask = mainTask.map((item,index)=>{
             return(
             <li key={index} className='list-none'>
-            <div className='flex justify-between items-center mb-4'>
-                <h3 className='text-black text-xl font-bold'>{index+1}) <b/> Title: {item.title}</h3>
-                <h4 className='text-zinc-700 font-semibold'>Description: {item.desc}</h4>
+            <div className='flex flex-col md:flex-row justify-between items-center mb-4'>
+                <h3 className='text-black text-xl font-bold mb-2 md:mb-0 md:mr-4 md:flex-shrink-0'>{index+1}) <b/> Title: {item.title}</h3>
+                <h4 className='whitespace-normal text-zinc-700 font-semibold mb-2 md:mb-0 md:mr-1'>Description: {item.desc}</h4>
                 <button onClick={()=>{
                     deleteHandler(index)
-                }} className='bg-red-600 rounded px-4 py-2'>Delete</button>
+                }} className='bg-red-600 rounded px-4 py-2 '>Delete</button>
             </div>
             </li>
             )
@@ -44,17 +46,17 @@ const page = () =>
         <>
             <h1 className='bg-black text-white p-4 text-center text-4xl font-bold'>Zain's Todo List</h1>
             <form onSubmit={submitHandler} className='text-center'>
-                <input type='text' className='text-2xl border-zinc-800 border-4 m-5 px-4 py-2' placeholder='Enter a task' value={title}
+                <input type='text' className='text-xl border-zinc-800 border-4 m-5 px-4 py-2' placeholder='Enter a task' value={title}
                 onChange={(e)=>{
                     settitle(e.target.value);
                 }}/>
-                <input type='text' className='text-2xl border-zinc-800 border-4 m-5 px-4 py-2' placeholder='Enter description' value={desc}
+                <input type='text' className='text-xl border-zinc-800 border-4 m-5 px-4 py-2' placeholder='Enter description' value={desc}
                 onChange={(e)=>{
                     setdesc(e.target.value)}}/>
-                <button className='bg-black text-white px-4 py-3 rounded font-bold ml-10 mb-5 text-2xl'>Add a Task</button>
+                <button className='bg-black text-white px-4 py-3 rounded font-bold ml-10 mb-5 text-xl'>Add a Task</button>
             </form>
             <hr/>
-            <div className='p-8 bg-cyan-200' >{renderTask}</div>   
+            <div className='p-8 bg-cyan-200'>{renderTask}</div>   
         </>
     )
 }
